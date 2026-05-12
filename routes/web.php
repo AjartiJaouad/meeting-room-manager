ecaa9ab86d01b2b7d9c4c460306aa9dd0e851b31
ajartijaouad <ajartijaouad14@gmail.com>
2026-05-12 13:17:18 +0100

    feat: setup protected admin routes for services and salles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SalleController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+    Route::resource('services', ServiceController::class);
+    Route::resource('salles', SalleController::class);
+});
 require __DIR__.'/auth.php';
